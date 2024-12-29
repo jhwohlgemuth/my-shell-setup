ab5aa25af06bba4f8fb967c1331d4776e8acd21a
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-12-28 19:49:43 -0500

    feat: Update subdomain to shell; beta install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 #! /bin/bash
 set -e
 
+requires \
+    git \
+    gum \
+    stow
 main() {
-    echo "UNDER CONSTRUCTION!!!"
+    local DOTFILES="${GOLD_DOTFILES:-"${HOME}/.dotfiles"}"
+    local TITLE="Installation $(gum style --foreground "${COLOR}" 'Complete')"
+    git clone https://github.com/jhwohlgemuth/my-shell-setup.git "${DOTFILES}"
+    stow --dir "${DOTFILES}" --target "${HOME}" --stow \
+        git \
+        neovim \
+        ohmyposh \
+        powershell \
+        powerlevel10k
+    gum style \
+        --border normal \
+        --border-foreground "${COLOR}" \
+        --margin "1" \
+        --padding "1 2" \
+        "${TITLE}"
 }
 main "$@"
