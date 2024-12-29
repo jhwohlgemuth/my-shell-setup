78044035f8f33e59ffda690aacae9576e51d2213
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2024-12-29 14:03:04 -0500

    feat: Refine and parameterize install script

--- install.sh
+++ install.sh
@@ -1,20 +1,48 @@
 #! /bin/bash
+# shellcheck disable=SC2155
 set -e
 
-requires \
-    git \
-    gum \
-    stow
 main() {
-    local DOTFILES="${GOLD_DOTFILES:-"${HOME}/.dotfiles"}"
+    local DEFAULT="gold git neovim ohmyposh powershell powerlevel10k"
+    local DIR="${GOLD_DOTFILES:-"${HOME}/.dotfiles"}"
     local TITLE="Installation $(gum style --foreground "${COLOR}" 'Complete')"
-    git clone https://github.com/jhwohlgemuth/my-shell-setup.git "${DOTFILES}"
-    stow --dir "${DOTFILES}" --target "${HOME}" --stow \
-        git \
-        neovim \
-        ohmyposh \
-        powershell \
-        powerlevel10k
+    #
+    # Create array of packages from arguments
+    #
+    if [[ -z "$1" ]]; then
+        # shellcheck disable=SC2206
+        PACKAGES=($DEFAULT)
+    else
+        PACKAGES=("$@")
+    fi
+    if [[ -e "${DIR}" ]] && [[ -d "${DIR}" ]]; then
+        #
+        # Update Git repository
+        #
+        git -C "${DIR}" pull origin main
+    else
+        #
+        # Clone repository
+        #
+        git clone https://github.com/jhwohlgemuth/my-shell-setup.git "${DIR}"
+    fi
+    #
+    # Stow packages
+    #
+    for PACKAGE in "${PACKAGES[@]}"; do
+        case "${PACKAGE}" in
+            gold)
+                local TARGET="/usr/local/bin"
+                ;;
+            *)
+                local TARGET="${HOME}"
+                ;;
+        esac
+        stow --dir "${DIR}" --target "${TARGET}" --stow "${PACKAGE}"
+    done
+    #
+    # Print completion message
+    #
     gum style \
         --border normal \
         --border-foreground "${COLOR}" \
